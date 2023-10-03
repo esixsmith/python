1be2dd4aa3108bab6f3ee623cd22b2f257447352
esixsmith <146867639+esixsmith@users.noreply.github.com>
2023-10-03 17:44:31 +0100

    Add files via upload

---
+++ school.py
@@ -0,0 +1,27 @@
+mark = int(input("what is your mark"))
+
+if mark <90:
+    if mark<80:
+        if mark<70:
+            if mark<60:
+                if mark<50:
+                    if mark<40:
+                        if mark<30:
+                            if mark<30:
+                                print("H8")
+                        else:
+                            print("H7")
+                    else:
+                        print("H6")
+                else:
+                    print("H5")
+            else:
+                print("H4")
+        else:
+            print("H3")
+    else:
+        print("H2")
+else:
+    print("H1")
+    
+    
